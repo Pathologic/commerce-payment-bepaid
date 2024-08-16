e17d10fa0c553eaa48eb3d54e0ef8e77af574a4a
Pathologic <m@xim.name>
2024-08-16 20:08:29 +0300

    refactor lexicons

--- assets/plugins/commerce/lang/be/bepaid.inc.php
+++ assets/plugins/commerce/lang/be/bepaid.inc.php
@@ -3,8 +3,8 @@
 setlocale(LC_ALL, 'ru_RU.UTF-8');
 
 return [
-    'bepaid.caption' => 'Оплата онлайн (Bepaid)',
-    'bepaid.link_caption' => 'Ссылка на оплату',
+    'bepaid.caption' => 'Аплата онлайн (Bepaid)',
+    'bepaid.link_caption' => 'Спасылка на аплату',
     'bepaid.error.empty_client_credentials' => 'Пазначце зьвесткі крамы ў наладах плагіна!',
     'bepaid.order_description' => 'Аплата замовы'
 ];
